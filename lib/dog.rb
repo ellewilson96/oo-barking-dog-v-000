a4fd0cc06650bb1792bcdc681dd6f1bfa01b5922
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-25 17:16:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Dog
+
+  def initialize(name, bark ="woof!")
+    @name = name
+    @bark = bark
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    @bark
+  end
+
+  def bark=(new_bark)
+  @bark = new_bark
+end
+
+
+end
